6d0957fe6d1e516195d3f070805d3e5eca061f2e
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-03-10 19:19:39 -0300

    feat: Challenge two completed

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT
+	COUNT(DISTINCT m.tracks) AS cancoes,
+  COUNT(DISTINCT art.artist_name) AS artistas,
+  COUNT(DISTINCT alb.title) AS albuns
+FROM musics AS m
+INNER JOIN albuns AS alb
+ON alb.album_id = m.album_id
+INNER JOIN artist AS art
+ON art.artist_id = alb.artist_id;
